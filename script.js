56a91c30e6ab5d308f671c6300f86a794b9aa6c0
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-10-31 08:45:38 +0100

    async fetch

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-function generateJoke() {
+jokeBtn.addEventListener("click", () => generateJoke());
+
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
 }
